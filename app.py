ba7782e05a5d4f6da90f0878d80548197391c6dd
Drew Miley <drewmiley92@gmail.com>
2020-07-01 00:46:43 +0100

    Updates with movies

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return {'hello': 'world'}
+movies = [
+    {
+        "name": "The Shawshank Redemption",
+        "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
+        "genres": ["Drama"]
+    },
+    {
+       "name": "The Godfather ",
+       "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
+       "genres": ["Crime", "Drama"]
+    }
+]
 
+@app.route('/movies')
+def hello():
+    return jsonify(movies)
 
 app.run()
