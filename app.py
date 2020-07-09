9108a9c8c63c554b8dc61af3b4690e4fbb8f1d2f
Drew Miley <drewmiley92@gmail.com>
2020-07-09 01:03:13 +0100

    Adds Login route

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from database.db import initialize_db
 from resources.routes import initialize_routes
 
 app = Flask(__name__)
+app.config.from_envvar('ENV_FILE_LOCATION')
 api = Api(app)
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/movie-bag'
--- resources/auth.py
+++ resources/auth.py
@@ -1,5 +1,7 @@
-from flask import request
+from flask import Response, request
+from flask_jwt_extended import create_access_token
 from flask_restful import Resource
+import datetime
 from database.models import User
 
 
@@ -11,3 +13,16 @@ class SignupApi(Resource):
         user.save()
         id = user.id
         return {'id': str(id)}, 200
+    
+
+class LoginApi(Resource):
+    def post(self):
+        body = request.get_json()
+        user = User.objects.get(email=body.get('email'))
+        authorized = user.check_password(body.get('password'))
+        if not authorized:
+            return {'error': 'Email or password invalid'}, 401
+
+        expires = datetime.timedelta(days=7)
+        access_token = create_access_token(identity=str(user.id), expires_delta=expires)
+        return {'token': access_token}, 200
